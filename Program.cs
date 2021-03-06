b458b20a32089c8b33f95f63b3b6294f740d1d6e
Luiz Henrique <lmenezesmessias@gmail.com>
2021-03-06 15:59:29 -0300

    Finalização

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cebola = Console.ReadLine();
+
+            string novaCebola = cebola.Replace("r", "l").Replace("R", "L");
+
+            Console.WriteLine(novaCebola);
+        }
+    }
+}
