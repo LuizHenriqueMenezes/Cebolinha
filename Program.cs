a4e549aba4d1d3e1089f7cae75d21de3828eb18c
Diego Neri <diego@diegoneri.com>
2021-03-12 18:58:07 -0300

    Adiciona regras de RR e adiciona mensagem
    Se o programa não contar com uma mensagem inicial, o usuário não saberá o que fazer com ele.

--- Program.cs
+++ Program.cs
@@ -6,9 +6,10 @@ namespace Cebolinha
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Escreva uma frase, para simular a dislalia do Cebolinha: ")
             string cebola = Console.ReadLine();
-
-            string novaCebola = cebola.Replace("r", "l").Replace("R", "L");
+            
+            string novaCebola = cebola.Replace("r","l").Replace("rr","l").Replace("rR","l").Replace("R","L").Replace("RR","L").Replace("Rr","L");
 
             Console.WriteLine(novaCebola);
         }
